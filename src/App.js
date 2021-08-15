16df86867987318655d2c176a6bc7688753a2bee
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-08-15 13:53:40 +0600

    add user login and chat feed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { ChatEngine } from "react-chat-engine";
 
-import './App.css';
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
 
-function App() {
+const projectID = "f9b11919-25bf-4c1a-b66d-361b66230ce2";
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div>
-      hello
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
 
 export default App;
